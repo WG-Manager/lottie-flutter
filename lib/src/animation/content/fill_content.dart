74ec93345dc9745e194bd0f97ef720d70079f10c
burakimdatt <b.imdat@ingot.io>
2026-04-10 10:09:38 +0300

    Update dependencies and enhance FillContent class for PathFillType (#413)
    This pull request updates the `FillContent` class to ensure that the
    fill type for paths is handled correctly and consistently. The main
    change is the introduction and application of the `_fillType` property,
    which ensures that the fill type from the `ShapeFill` is always used
    when constructing and drawing paths.
    
    **Fill type handling improvements:**
    
    * Added a `_fillType` property to the `FillContent` class, initialized
    from the `ShapeFill` object in the constructor.
    [[1]](diffhunk://#diff-7d2b2fe505f240df037e7675d921b3b20685a3864488fd0e1b7f42e7c6018640R26)
    [[2]](diffhunk://#diff-7d2b2fe505f240df037e7675d921b3b20685a3864488fd0e1b7f42e7c6018640L39-R43)
    * Set the `_path.fillType` property to `_fillType` before adding
    subpaths in both the drawing and bounds calculation methods, ensuring
    consistent fill behavior.
    [[1]](diffhunk://#diff-7d2b2fe505f240df037e7675d921b3b20685a3864488fd0e1b7f42e7c6018640R119)
    [[2]](diffhunk://#diff-7d2b2fe505f240df037e7675d921b3b20685a3864488fd0e1b7f42e7c6018640R139)
    
    **Code organization:**
    
    * Updated import statements for clarity and consistency.
    
    ---------
    
    Co-authored-by: Xavier Hainaux <xavier.hainaux@gmail.com>

--- lib/src/animation/content/fill_content.dart
+++ lib/src/animation/content/fill_content.dart
@@ -1,5 +1,7 @@
 import 'dart:ui';
+
 import 'package:vector_math/vector_math_64.dart';
+
 import '../../l.dart';
 import '../../lottie_drawable.dart';
 import '../../lottie_property.dart';
@@ -22,9 +24,7 @@ import 'path_content.dart';
 class FillContent implements DrawingContent, KeyPathElementContent {
   final Path _path = Path();
   final BaseLayer layer;
-  @override
-  final String? name;
-  final bool _hidden;
+  final ShapeFill _fill;
   final List<PathContent> _paths = <PathContent>[];
   late final BaseKeyframeAnimation<Color, Color> _colorAnimation;
   late final BaseKeyframeAnimation<int, int> _opacityAnimation;
@@ -34,9 +34,10 @@ class FillContent implements DrawingContent, KeyPathElementContent {
   double _blurMaskFilterRadius = 0;
   DropShadowKeyframeAnimation? dropShadowAnimation;
 
-  FillContent(this.lottieDrawable, this.layer, ShapeFill fill)
-    : name = fill.name,
-      _hidden = fill.hidden {
+  @override
+  String? get name => _fill.name;
+
+  FillContent(this.lottieDrawable, this.layer, this._fill) {
     var blurEffect = layer.blurEffect;
     if (blurEffect != null) {
       _blurAnimation = blurEffect.blurriness.createAnimation()
@@ -52,16 +53,14 @@ class FillContent implements DrawingContent, KeyPathElementContent {
       );
     }
 
-    if (fill.color == null || fill.opacity == null) {
+    if (_fill.color == null || _fill.opacity == null) {
       return;
     }
 
-    _path.fillType = fill.fillType;
-
-    _colorAnimation = fill.color!.createAnimation();
+    _colorAnimation = _fill.color!.createAnimation();
     _colorAnimation.addUpdateListener(onValueChanged);
     layer.addAnimation(_colorAnimation);
-    _opacityAnimation = fill.opacity!.createAnimation();
+    _opacityAnimation = _fill.opacity!.createAnimation();
     _opacityAnimation.addUpdateListener(onValueChanged);
     layer.addAnimation(_opacityAnimation);
   }
@@ -82,7 +81,7 @@ class FillContent implements DrawingContent, KeyPathElementContent {
 
   @override
   void draw(Canvas canvas, Matrix4 parentMatrix, {required int parentAlpha}) {
-    if (_hidden) {
+    if (_fill.hidden) {
       return;
     }
     L.beginSection('FillContent#draw');
@@ -112,6 +111,7 @@ class FillContent implements DrawingContent, KeyPathElementContent {
     }
 
     _path.reset();
+    _path.fillType = _fill.fillType;
     for (var i = 0; i < _paths.length; i++) {
       _path.addPath(_paths[i].getPath(), Offset.zero);
     }
@@ -131,6 +131,7 @@ class FillContent implements DrawingContent, KeyPathElementContent {
   @override
   Rect getBounds(Matrix4 parentMatrix, {required bool applyParents}) {
     _path.reset();
+    _path.fillType = _fill.fillType;
     for (var i = 0; i < _paths.length; i++) {
       _path.addPath(
         _paths[i].getPath(),
